154d254a47c1d58063b7067d8b35b368c0b5ed7e
Aswin-art <aswinarung2@gmail.com>
2022-10-26 10:21:57 +0700

    asndusdn

--- SPEEDTEST_MODULE/C3/main.js
+++ SPEEDTEST_MODULE/C3/main.js
@@ -1,5 +1,11 @@
 const scope = document.querySelector('.scope')
+const topEl = document.querySelector('.top')
+const bottomEl = document.querySelector('.bottom')
+const leftEl = document.querySelector('.left')
+const rightEl = document.querySelector('.right')
 document.addEventListener('mousemove', (e) => {
-    scope.style.left = e.pageX + 'px'
-    scope.style.top = e.pageY + 'px'
+    topEl.style.height = e.clientY - 200 + 'px'
+    bottomEl.style.height = innerHeight - e.clientY - 200 + 'px'
+    leftEl.style.width = e.clientX - 200 + 'px'
+    rightEl.style.width = innerWidth - e.clientX - 200 + 'px'
 })
