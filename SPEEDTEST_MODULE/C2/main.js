aa6116daa9a5d4fac1be44fd891da85b52f642b1
Aswin-art <aswinarung2@gmail.com>
2022-10-26 10:07:01 +0700

    as

--- SPEEDTEST_MODULE/C2/main.js
+++ SPEEDTEST_MODULE/C2/main.js
@@ -1,6 +1,21 @@
-const slider = document.querySelector('.slider')
-const img = document.querySelector('image')
-slider.addEventListener('input', () => {
-    let value = slider.value
-    img.style.filter = grayscale('100%') 
+const image = document.querySelector('.image')
+const container = document.querySelector('.container')
+const bounding = image.getBoundingClientRect()
+
+function move(e){
+    let x = e.pageX - bounding.left
+    let y = e.pageY - bounding.top
+    image.style.width = x + 'px' 
+}
+
+container.addEventListener('mousedown', () => {
+    document.addEventListener('mousemove', (e) => {
+        move(e)
+    })
+})
+
+container.addEventListener('mouseup', () => {
+    document.addEventListener('mousemove', (e) => {
+        move(e)
+    })
 })
